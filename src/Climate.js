d64ff3a4e6ade4672d5b2f056a6f752e4f15f362
Nathasha <nsoares@corpcloud.com.au>
2022-11-22 11:03:04 +0800

    Styling my ns react app

--- src/Climate.js
+++ src/Climate.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function Climate() {
+function Climate(props) {
+  const [temperature, setTemperature] = useState(null);
 
-    
+  function showTemperature(response) {
+    setTemperature(response.data.main.temp);
+  }
+
+  if (temperature) {
+    return (
+      <p>
+        The current temperature in {props.city} is {Math.round(temperature)}°C.
+      </p>
+    );
+  } else {
+    const apiKey = "d122489789ce9e01ba81bb0f4a64028b";
+    const units = "metric";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showTemperature);
+
+    <p>Loading temperature for {props.city}...</p>;
+  }
 }
 
 export default Climate;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./App.css";
-// import Cities from "./Cities.js";
+
 import Climate from "./Climate.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Climate />
+    <h1>React AJAX</h1>
+    <Climate city="Perth" />
+    <Climate city="Itabira" />
+    <Climate city="Antarctica" />
   </React.StrictMode>
 );
